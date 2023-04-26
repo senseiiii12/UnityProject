a6a40b56f67e2256afa97e5f983e7e833a98a80f
senseiiii12 <117999420+senseiiii12@users.noreply.github.com>
2023-04-26 22:13:34 +0300

    qqq

--- My project/Assets/scripts/CHscipt.cs
+++ My project/Assets/scripts/CHscipt.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CHscipt : MonoBehaviour
 {
-    public float speed = 8;
+    public float speed = 10;
     public float jumpvalue = 12;
     public float gravity = 20;
     private Vector3 moveDirection = Vector3.zero;
@@ -20,7 +20,7 @@ public class CHscipt : MonoBehaviour
         CharacterController controller = GetComponent<CharacterController>();
         if (controller.isGrounded)
         {
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"),0,0);
             moveDirection = transform.TransformDirection(moveDirection);
             moveDirection *= speed;
             if (Input.GetButton("Jump"))
--- My project/Assets/scripts/Enemy.cs
+++ My project/Assets/scripts/Enemy.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    PlayerScript player;
+    CHscipt player;
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, 10);
-        player = GameObject.FindObjectOfType<PlayerScript>();
+        Destroy(gameObject, 5);
+        player = GameObject.FindObjectOfType<CHscipt>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(transform.position, player.transform.position) < 2)
+        if (Vector3.Distance(transform.position, player.transform.position) < 1)
         {
             Destroy(gameObject);
         }
